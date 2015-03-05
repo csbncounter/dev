e18a5c618ecca5b9e2b2cbe98cb83f4e197fb69f
Ben Deibert <backpackingat@gmail.com>
2015-03-05 14:58:37 -0500

    edited js file

--- js/newsImages.js
+++ js/newsImages.js
@@ -4,13 +4,13 @@ function randomImages() {
 	var pics = ['/img/indexImgs/frisbee.jpg', '/img/indexImgs/group.jpg', '/img/indexImgs/meteor.jpg', '/img/indexImgs/prayer.jpg', '/img/indexImgs/rifle.jpg' ];
 	
 	$('header').css({'background' : 'url('+pics[Math.floor(Math.random() * pics.length)] + ') no-repeat', 
-		'background-attachment: scroll',
-		'background-position: center center',
-    'background-repeat: none',
-    '-webkit-background-size: cover',
-    '-moz-background-size: cover',
-    'background-size: cover',
-    '-o-background-size: cover',
+		'background-attachment' : 'scroll',
+		'background-position' : 'center center',
+    'background-repeat' : 'none',
+    '-webkit-background-size' : 'cover',
+    '-moz-background-size' : 'cover',
+    'background-size' : 'cover',
+    '-o-background-size' : 'cover',
     });
 }// end script
 
