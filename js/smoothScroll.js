8966d56f60e63982d452e307074d14bab2c141bf
Ben Deibert <backpackingat@gmail.com>
2015-03-26 17:17:16 -0400

    Added Smooth Scroll JS

---
+++ js/smoothScroll.js
@@ -0,0 +1,18 @@
+// js to assist in smoothly scrolling through internal page hrefs
+// Author: Ben Deibert
+// Credit: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
