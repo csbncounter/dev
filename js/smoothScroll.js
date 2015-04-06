b0736666b5724a44ec7ef5201118a1f61f1ec4b1
Ben Deibert <backpackingat@gmail.com>
2015-04-06 11:12:32 -0400

    added cite and explanation on Script page.

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,7 +1,8 @@
 // js to assist in smoothly scrolling through internal page hrefs
 // Author: Ben Deibert
 // Credit: https://css-tricks.com/snippets/jquery/smooth-scrolling/
-/*
+/* This function also picks up the internal hrefs that the index -> protfolio section needs. 
+Due to that, with this script, the internal hrefs 'pop up windows' didn't work. 
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -19,6 +20,7 @@ $(function() {
 */
 
 // both the target and button need to have rel="relativeanchor" before it will work... 
+//Cited: http://jsfiddle.net/francescov/4DcNH/
 $(document).ready(function() {
 	$('a[rel="relativeanchor"]').click(function(){
 	    $('html, body').animate({
